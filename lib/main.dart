27c30c6f4a12db1f829fa9fbf1b90399e7fbe63b
Dreyfi Ferreira <dreyfi.sferreira@gmail.com>
2019-06-16 22:19:13 -0300

    Iniciando o layout; Input de adição de tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,49 @@ void main() {
 }
 
 class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
 
   List _toDoList = [];
 
   @override
-  _HomeState createState() => _HomeState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: "Nova tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("Add"),
+                  textColor: Colors.white,
+                  onPressed: () {},
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
@@ -34,13 +72,6 @@ class Home extends StatefulWidget {
     } catch (e){
       return null;
     }
-  }
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
 
